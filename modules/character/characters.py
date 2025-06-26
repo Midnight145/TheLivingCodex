b4a35a00c129a4c512acb2449259acd60fbd7514
Midnight <23142346+Midnight145@users.noreply.github.com>
2025-06-26 01:07:01 -0500

    update help text

--- modules/character/characters.py
+++ modules/character/characters.py
@@ -57,16 +57,15 @@ class CharacterModule(commands.Cog):
     """
 
     config_help_str = """
+    ## Configuration:
     -----------------
-   
-   ** Configuration:**
-   
-   User Configuration:
-   `{prefix}config <option> <value>` - Configure user settings. Options include:
-    - `autoreact` - Enable or disable automatic reactions to proxied messages. Set to `true` or `false`.
     
-    Server Configuration:
-    `{prefix}prefix <new_prefix>` - Change the bot's prefix for the server. This will change the prefix for all commands.
+    **User Configuration:**
+        `{prefix}config <option> <value>` - Configure user settings. Options include:
+            `autoreact` - Enable or disable automatic reactions to proxied messages. Set to `true` or `false`.
+    
+    **Server Configuration:**
+        `{prefix}prefix <new_prefix>` - Change the bot's prefix for the server. This will change the prefix for all commands.
 """
 
     @commands.command(aliases=["delete"])
